534b299827bfb3de95eee6107d22db27963162f0
Antoine Vaillant <a.vaillant.sn@gmail.com>
2015-04-07 08:04:04 +0000

    modif main gui

--- gui/main.c
+++ gui/main.c
@@ -1,6 +1,67 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
+typedef struct {
+    int owner;           /*!<  Joueur controlant la case */
+    unsigned int nbDames;   /*!<  Nombre de dames sur la case (non utilise si
+                             `owner == NOBODY.`) */
+} Square;
+
+typedef struct {
+    Square board[24];           /*!< Les 24 cases du tableau, en partant du bord blanc. */
+    unsigned int bar[2];
+    unsigned int out[2];
+    unsigned int whiteScore;    /*!< Score du joueur blanc */
+    unsigned int blackScore;    /*!< Score du joueur noir */
+    unsigned int turn;          /*!< Numero du tour en cours (initialise a 0) */
+    unsigned int stake;			/*!< Mise courante de la partie */
+} SGameState;
+
+SDL_Renderer * renderer;
+
+
+void setBoardTokens(const SGameState * const state, SDL_Rect noirs[15] , SDL_Rect rouges[15])
+{
+    //Tableau d'equivalence entre les cases et leurs positions en pixels
+    int equivalence_x[12] = {1080, 1022, 962, 902, 842, 722, 558, 500, 385, 327, 270, 212};
+    
+    //Tableau d'equivalence entre la position sur une case et la position en pixel correspondante
+    int equivalence_y[5] = {645, 581, 517, 453, 389};
+    int equivalence_y2[5] = {50, 114, 178, 242, 306};
+    
+    int i = 0, j = 0, cptN = 0, cptR = 0;
+    
+    //Parcours du SGameState
+    for (i = 0; i<24; i++)
+    {
+        if (state->board[i].owner == 0)
+        {
+            for (j = 0; j<state->board[i].nbDames; j++)
+            {
+                noirs[cptN].x = equivalence_x[i];
+                
+                if(i<13) noirs[cptN].y = equivalence_y[j];
+                else noirs[cptN].y = equivalence_y2[j];
+                
+                cptN++;
+            }
+        }
+        else
+        {
+            for (j = 0; j<state->board[i].nbDames; j++)
+            {
+                rouges[cptR].x = equivalence_x[i];
+                
+                if(i<13) noirs[cptR].y = equivalence_y[j];
+                else noirs[cptR].y = equivalence_y2[j];
+                
+                cptN ++;
+            }
+        }
+    }
+    SDL_RenderPresent(renderer);
+}
+
 int main(int argc, char** argv)
 {
     /* ============****** VARIABLES ******============ */
@@ -20,7 +81,7 @@ int main(int argc, char** argv)
     if (pWindow == NULL) SDL_ShowSimpleMessageBox(0, "Window init error", SDL_GetError(), pWindow);
     
     /* ============****** GESTION DES IMAGES ******============ */
-    SDL_Renderer * renderer = SDL_CreateRenderer(pWindow, -1, 0);
+    renderer = SDL_CreateRenderer(pWindow, -1, 0);
     if (renderer == NULL) SDL_ShowSimpleMessageBox(0, "Renderer init error", SDL_GetError(), pWindow);
     
     SDL_Surface * image = SDL_LoadBMP("plateau.bmp");
@@ -46,41 +107,39 @@ int main(int argc, char** argv)
     
     SDL_Rect r1 = { 1080, 645, 64, 64 }; //Jan 1
     SDL_Rect r2 = { 1080, 581, 64, 64 }; //Jan 1
-    
-    SDL_Rect n1 = { 722, 645, 64, 64 }; //Jan 6
-    SDL_Rect n2 = { 722, 581, 64, 64 }; //Jan 6
-    SDL_Rect n3 = { 722, 517, 64, 64 }; //Jan 6
-    SDL_Rect n4 = { 722, 453, 64, 64 }; //Jan 6
-    SDL_Rect n5 = { 722, 389, 64, 64 }; //Jan 6
-    
-    SDL_Rect n6 = { 500, 645, 64, 64 }; //Jan 8
-    SDL_Rect n7 = { 500, 581, 64, 64 }; //Jan 8
-    SDL_Rect n8 = { 500, 517, 64, 64 }; //Jan 8
-    
     SDL_Rect r3 = { 212, 645, 64, 64 }; //Jan 12
     SDL_Rect r4 = { 212, 581, 64, 64 }; //Jan 12
     SDL_Rect r5 = { 212, 517, 64, 64 }; //Jan 12
     SDL_Rect r6 = { 212, 453, 64, 64 }; //Jan 12
     SDL_Rect r7 = { 212, 389, 64, 64 }; //Jan 12
-    
-    SDL_Rect n9 = { 212, 50, 64, 64 }; //Jan 13
-    SDL_Rect n10 = { 212, 114, 64, 64 }; //Jan 13
-    SDL_Rect n11 = { 212, 178, 64, 64 }; //Jan 13
-    SDL_Rect n12 = { 212, 242, 64, 64 }; //Jan 13
-    SDL_Rect n13 = { 212, 306, 64, 64 }; //Jan 13
-    
     SDL_Rect r8 = { 500, 50, 64, 64 }; //Jan 17
     SDL_Rect r9 = { 500, 114, 64, 64 }; //Jan 17
     SDL_Rect r10 = { 500, 178, 64, 64 }; //Jan 17
-    
     SDL_Rect r11 = { 722, 50, 64, 64 }; //Jan 19
     SDL_Rect r12 = { 722, 114, 64, 64 }; //Jan 19
     SDL_Rect r13 = { 722, 178, 64, 64 }; //Jan 19
     SDL_Rect r14 = { 722, 242, 64, 64 }; //Jan 19
     SDL_Rect r15 = { 722, 306, 64, 64 }; //Jan 19
     
+    SDL_Rect rouges[15] = {r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15};
+    
+    SDL_Rect n1 = { 722, 645, 64, 64 }; //Jan 6
+    SDL_Rect n2 = { 722, 581, 64, 64 }; //Jan 6
+    SDL_Rect n3 = { 722, 517, 64, 64 }; //Jan 6
+    SDL_Rect n4 = { 722, 453, 64, 64 }; //Jan 6
+    SDL_Rect n5 = { 722, 389, 64, 64 }; //Jan 6
+    SDL_Rect n6 = { 500, 645, 64, 64 }; //Jan 8
+    SDL_Rect n7 = { 500, 581, 64, 64 }; //Jan 8
+    SDL_Rect n8 = { 500, 517, 64, 64 }; //Jan 8
+    SDL_Rect n9 = { 212, 50, 64, 64 }; //Jan 13
+    SDL_Rect n10 = { 212, 114, 64, 64 }; //Jan 13
+    SDL_Rect n11 = { 212, 178, 64, 64 }; //Jan 13
+    SDL_Rect n12 = { 212, 242, 64, 64 }; //Jan 13
+    SDL_Rect n13 = { 212, 306, 64, 64 }; //Jan 13
     SDL_Rect n14 = { 1080, 50, 64, 64 }; //Jan 24
     SDL_Rect n15 = { 1080, 114, 64, 64 }; //Jan 24
+    
+    SDL_Rect noirs[15] = {n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14, n15};
     
     //Jetons Noirs
     SDL_RenderCopy(renderer, texture_jeton_noir, NULL, &n1);
@@ -124,7 +183,7 @@ int main(int argc, char** argv)
     {
         while (continuer == 1)
         {
-            SDL_WaitEvent(&event);
+            SDL_PollEvent(&event);
             
             switch(event.type)
             {
@@ -132,7 +191,6 @@ int main(int argc, char** argv)
                     continuer = 0;
                     break;
             }
-            apply_surface(0,10, NULL, pWindow);
         }
     }
     else
@@ -142,26 +200,11 @@ int main(int argc, char** argv)
     
     SDL_DestroyTexture(texture);
     SDL_FreeSurface(image);
+    SDL_FreeSurface(jeton_noir);
+    SDL_FreeSurface(jeton_rouge);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(pWindow);
     SDL_Quit();
     
     return 0;
-}
-
-void draw_surface(int srcX, int srcY, int dstX, int dstY, int width, int height, SDL_Surface *source, SDL_Surface *destination)
-{
-    SDL_Rect src;
-    src.x = srcX;
-    src.y = srcY;
-    src.w = width;
-    src.h = height;
-    
-    SDL_Rect dst;
-    dst.x = dstX;
-    dst.y = dstY;
-    dst.w = width;
-    dst.h = height;
-    
-    SDL_BlitSurface(source, &src, destination, &dst);
 }
